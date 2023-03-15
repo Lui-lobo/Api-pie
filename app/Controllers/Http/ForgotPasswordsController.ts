0be6075f4260bf004b6619acb4d98b48e06b529d
LUIZ HENRIQUE CARVALHAS LOBO DE OLIVEIRA <luiz.hcloliveira@senacsp.edu.br>
2023-03-14 22:00:16 -0300

    atualizando o sistema de envio de email de recuperação

--- app/Controllers/Http/ForgotPasswordsController.ts
+++ app/Controllers/Http/ForgotPasswordsController.ts
@@ -2,21 +2,44 @@
 import Mail from '@ioc:Adonis/Addons/Mail'
 
 import UserModel from 'App/Models/UserModel'
+const user = new UserModel()
+
 
 export default class ForgotPasswordsController {
 
+    
    async sendResetLinkEmail({request, session, response}) {
-    const emailUser = request.only('login')
-
+    const emailUser = await request.input('login')
     console.log(emailUser)
 
-        Mail.send((message) => {
-            message
-                .from('teste@example.com')
-                .to('')
-                .subject('Recover Pass')
-                
-        })
-   }
+        if(emailUser == null || emailUser == "" || emailUser === "" || emailUser == undefined) {
+            console.log('deu ruim')
+        } else {
+            console.log('e um email')
+
+            const User = await UserModel.findBy('login', emailUser)
+
+            console.log(User?.Login)
+
+            if(User?.Login == undefined || User?.Login == null) {
+                console.log('o usuario não encontrado no sistema')
+            } else if (User?.Login === emailUser) {
+                console.log('o usuario existe, email de recuperação enviado')
+
+                Mail.send((message) => {
+                message
+                    .from('teste@example.com')
+                    .to('')
+                    .subject('Recover Pass')
+                    .htmlView('emails/recover', { name: 'Luiz' })
+                })
+
+            } else {
+                console.log('o usuario não existe')
+            }
+        }
+
+        }
+    }
    
-}
+
